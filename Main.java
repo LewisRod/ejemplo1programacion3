402947edcece2960c9651fe4f2b5accebe8faf1d
Lewis Rodriguez <lewis_rodriguez1@ucne.edu.do>
2026-01-19 18:51:23 -0400

    Agregando mi primer repositorio

---
+++ Main.java
@@ -0,0 +1,12 @@
+package ejemplo1programacion3;
+
+public class Main {
+    static void main(){
+        int x = 5;
+        int y = 5;
+
+        int resultado = x + y;
+
+        System.out.println("La suma la: " + resultado);
+    }
+}
